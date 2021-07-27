831bdf4b0696982ac867927361c133c15c8697be
Madat Bay <medet.bayramov22@gmail.com>
2021-07-28 02:39:58 +0400

    api detail view resolve fixed

--- board/urls.py
+++ board/urls.py
@@ -3,13 +3,10 @@ from rest_framework import routers
 
 from . import views
 
-app_name = "board"
-
 router = routers.DefaultRouter()
 router.register(r'news', views.NewsViewSet)
 router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("board.urls", namespace="board")),
+    path("", include("board.urls")),
 ]
