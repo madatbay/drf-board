059775a2eb9152186faa63e59705e29a6b7ec67d
Madat Bay <medet.bayramov22@gmail.com>
2021-07-28 02:26:49 +0400

    api endpoint view created

--- board/views.py
+++ board/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Comment, News
+from .serializers import CommentSerializer, NewsSerializer
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all().order_by("-created_at")
+    serializer_class = NewsSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("-created_at")
+    serializer_class = CommentSerializer
