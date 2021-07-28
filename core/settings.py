9e53aeff190c9ea4d7b3a9fd2ba20b232d1ff472
Madat Bay <medet.bayramov22@gmail.com>
2021-07-29 03:08:48 +0400

    static_root path fixed

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
