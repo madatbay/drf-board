a8092e216c394832c9529ad17e3bc17362295442
Madat Bay <medet.bayramov22@gmail.com>
2021-07-29 03:56:54 +0400

    admin: list_display property added

--- board/admin.py
+++ board/admin.py
@@ -4,11 +4,13 @@ from .models import News, Comment
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title", "link", "author", "upvotes", "created_at")
     search_fields = ("title", "author", "created_at")
     list_filter = ("author", "created_at")
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    list_display = ("news", "author_name", "content", "created_at")
     search_fields = ("author_name", "content")
     list_filter = ("author_name", "created_at")
